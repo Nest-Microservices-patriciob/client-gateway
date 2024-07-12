6377f119f3755c6ee2267c704a2a504448b30c26
Patricio Bonetto <patricio.bonetto@outlook.com>
2024-07-12 10:04:32 -0300

    added dockerfile

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -10,6 +10,13 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
     const rpcError = exception.getError();
 
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError.toString(),
+      });
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
